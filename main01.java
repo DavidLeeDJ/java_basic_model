28133701a630e38909af96fa8f4c288da9a16a82
이동주 <93064572+DavidLeeDJ@users.noreply.github.com>
2022-06-18 12:00:13 +0900

    main 정의와 변수 설정

---
+++ main01.java
@@ -0,0 +1,15 @@
+package basic_1;
+
+public class main01 {
+	public static void main(String[] args) {
+		
+		
+		System.out.println("hello world");
+		System.out.println("hi");
+		dod();
+	}
+
+	public static void dod() {
+		System.out.println("do code");
+}
+}
---
+++ variable02.java
@@ -0,0 +1,22 @@
+package basic_1;
+
+public class variable02 {
+	public static void main(String[] args) {
+		
+		//변수 선언하는 방법
+		int intAmout = 10;//뱐수 선언시 낙타표현법 처럼 대문자를 기점으로 가독성 쉽게 하기
+		String strCaompanyName = "samsung";//strCompayname 은 변수명 , 초기화라는 직업을 수행한다.
+		int intA = 0;
+		int firstValue = 10;
+		int secondValue = 20;
+		int sumValue = 0;
+		
+		sumValue = firstValue + secondValue;
+		System.out.println("합계"+ sumValue);
+		
+		
+		
+		
+	}
+
+}
